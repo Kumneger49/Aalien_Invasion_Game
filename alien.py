54b108542144122f16b243da985a225fcec9a96a
Kumneger49 <132818323+Kumneger49@users.noreply.github.com>
2024-02-15 05:07:01 -0800

    changed path for the alien image in alien.py

--- alien.py
+++ alien.py
@@ -6,7 +6,7 @@ class Alien(Sprite):
         super().__init__()
         self.var=0
         self.screen=ai.screen
-        self.image=pygame.image.load("C:\\Users\\User\\Desktop\\Python\\Alien_Invasion\\images\\Toy-576514_1280-_2_.bmp")
+        self.image=pygame.image.load("images\\Toy-576514_1280-_2_.bmp")
         self.rect=self.image.get_rect()
         self.rect.x=self.rect.width
         self.rect.y=self.rect.height
